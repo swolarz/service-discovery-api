21e6ca6079e07da1a34825c7f05f28e37873b3a8
swolarz <szymon.wol@vp.pl>
2019-06-13 23:43:12 +0200

    Initial commit Spring initializer base project

---
+++ src/main/java/com/put/swolarz/servicediscoveryapi/ServiceDiscoveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.put.swolarz.servicediscoveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryApiApplication.class, args);
+	}
+
+}
